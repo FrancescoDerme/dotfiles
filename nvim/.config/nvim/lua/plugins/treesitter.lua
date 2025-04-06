398f7cdefa56e555812d0a81868e6b78e23d90f4
Francesco Derme <francesco.derme02@gmail.com>
2025-04-06 18:26:27 +0200

    treesitter-textobjects no mappings

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,34 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+
     config = function()
         local config = require("nvim-treesitter.configs")
         config.setup({
-            ensure_installed = {"lua", "c", "cpp"},
+            ensure_installed = {
+                "lua",
+                "c",
+                "cpp",
+                "markdown",
+                "markdown_inline",
+                "bash",
+                "gitignore"
+            },
             highlight = {enable = true},
-            indent = {enable = true}
+            indent = {enable = true},
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<C-space>",
+                    node_incremental = "<C-space>",
+                    scope_incremental = false,
+                    node_decremental = "<bs>",
+                }
+            }
         })
     end
 }
