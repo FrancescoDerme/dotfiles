a96c277c61d7814c2cade3077fe7fecba3ff2a59
Francesco Derme <francesco.derme02@gmail.com>
2025-04-10 23:25:46 +0200

    Formatting

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -25,24 +25,25 @@ vim.keymap.set('n', '<M-l>', '<C-w>l', {desc = "Switch focus right"})
 -- Keymaps for tabs and splits
 vim.keymap.set('n', '<leader>t', ':tabnew <CR>', {desc = "Open new tab"})
 vim.keymap.set('n', '<leader>wv', ':vsplit <CR>', {desc = "Open vertical window"})
+vim.keymap.set('n', '<leader>wh', ':split <CR>', {desc = "Open horizontal window"})
 
 -- Neotree
 vim.keymap.set('n', '<leader>e', ':Neotree filesystem toggle left <CR>', {desc = "Toggle file tree"})
 
 -- Telescope
-vim.keymap.set('n', '<leader>ff', ':Telescope live_grep <CR>', {desc = "Find text in files in the current directory"})
-vim.keymap.set('n', '<leader>fb', ':Telescope current_buffer_fuzzy_find <CR>', {desc = "Find text in the current buffer"})
-vim.keymap.set('n', '<leader>fd', ':Telescope find_files <CR>', {desc = "Find files in the current directory"})
+vim.keymap.set('n', '<leader>fd', ':Telescope live_grep <CR>', {desc = "Text in directory"})
+vim.keymap.set('n', '<leader>fb', ':Telescope current_buffer_fuzzy_find <CR>', {desc = "Text in buffer"})
+vim.keymap.set('n', '<leader>ff', ':Telescope find_files <CR>', {desc = "File in directory"})
 
 -- Competitest
-vim.keymap.set('n', '<leader>ca', ':CompetiTest add_testcase <CR>', {desc = "Competitest add testcase"})
-vim.keymap.set('n', '<leader>ce', ':CompetiTest edit_testcase <CR>', {desc = "Competitest edit testcase"})
-vim.keymap.set('n', '<leader>cr', ':CompetiTest run <CR>', {desc = "Competitest run testcases"})
-vim.keymap.set('n', '<leader>cs', ':CompetiTest show_ui <CR>', {desc = "Competitest show ui"})
-vim.keymap.set('n', '<leader>cdt', ':CompetiTest receive testcases <CR>', {desc = "Competitest download testcases"})
-vim.keymap.set('n', '<leader>cdp', ':CompetiTest receive problem <CR>', {desc = "Competitest download problem"})
-vim.keymap.set('n', '<leader>cdc', ':CompetiTest receive contest <CR>', {desc = "Competitest download contest"})
+vim.keymap.set('n', '<leader>ca', ':CompetiTest add_testcase <CR>', {desc = "Add testcase"})
+vim.keymap.set('n', '<leader>ce', ':CompetiTest edit_testcase <CR>', {desc = "Edit testcase"})
+vim.keymap.set('n', '<leader>cr', ':CompetiTest run <CR>', {desc = "Run testcases"})
+vim.keymap.set('n', '<leader>cs', ':CompetiTest show_ui <CR>', {desc = "Show ui"})
+vim.keymap.set('n', '<leader>cdt', ':CompetiTest receive testcases <CR>', {desc = "Testcases"})
+vim.keymap.set('n', '<leader>cdp', ':CompetiTest receive problem <CR>', {desc = "Problem"})
+vim.keymap.set('n', '<leader>cdc', ':CompetiTest receive contest <CR>', {desc = "Contest"})
 
 -- Oil
-vim.keymap.set('n', '<leader>oe', ':Oil <CR>', {desc = "Oil file explorer"})
-vim.keymap.set('n', '<leader>of', ':Oil --float <CR>', {desc = "Floating oil file explorer"})
+vim.keymap.set('n', '<leader>oe', ':Oil <CR>', {desc = "File explorer"})
+vim.keymap.set('n', '<leader>of', ':Oil --float <CR>', {desc = "Floating file explorer"})
---
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,46 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			formatters_by_ft = {
+				cpp = { "clang_format" },
+				lua = { "stylua" },
+				--[[
+                javascript = { "prettier" },
+                typescript = { "prettier" },
+                javascriptreact = { "prettier" },
+                typescriptreact = { "prettier" },
+                svelte = { "prettier" },
+                css = { "prettier" },
+                html = { "prettier" },
+                json = { "prettier" },
+                yaml = { "prettier" },
+                markdown = { "prettier" },
+                graphql = { "prettier" },
+                python = { "isort", "black" },
+                ]]
+			},
+            formatters = {
+                clang_format = {
+                    prepend_args = {"--style={BasedOnStyle: google, IndentWidth: 4}"},
+                },
+            },
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			},
+		})
+
+		vim.keymap.set({ "n", "v" }, "<leader>p", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			})
+		end, { desc = "Format file or range" })
+	end,
+}
