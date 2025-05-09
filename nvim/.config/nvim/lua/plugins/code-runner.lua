a681bec4f3c032be6f59d0cd4f2a50c769269f3e
Francesco Derme <francesco.derme02@gmail.com>
2025-05-09 15:35:04 +0200

    Updated docs

--- nvim/.config/nvim/lua/plugins/code-runner.lua
+++ nvim/.config/nvim/lua/plugins/code-runner.lua
@@ -2,6 +2,7 @@ return {
 	"CRAG666/code_runner.nvim",
 	config = function()
 		require("code_runner").setup({
+			focus = false,
 			filetype = {
 				crunner = function(...)
 					vim.cmd("wincmd k")
