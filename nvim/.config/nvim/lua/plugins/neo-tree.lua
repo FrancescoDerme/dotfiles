08cf41f3104f341a7c48fc132ce9280654cc6aaf
Francesco Derme <francesco.derme02@gmail.com>
2025-05-21 11:36:32 +0200

    Relative line number inside neotree buffer

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -6,30 +6,26 @@ return {
 		"nvim-tree/nvim-web-devicons",
 		"MunifTanjim/nui.nvim",
 	},
-	--[[
-	opts = {
-		event_handlers = {
-			{
-				event = "neo_tree_buffer_enter",
-				handler = function()
-					vim.opt_local.relativenumber = true
-				end,
-			},
-		},
-	},
-    ]]
 	config = function()
 		require("neo-tree").setup({
+			event_handlers = {
+				{
+					event = "neo_tree_buffer_enter",
+					handler = function()
+						vim.opt.relativenumber = true
+					end,
+				},
+			},
+			--[[
 			window = {
-				--[[
 				mappings = {
 					["P"] = { "toggle_preview", config = { use_float = false, use_image_nvim = true } },
 					["l"] = "focus_preview",
 					["<C-b>"] = { "scroll_preview", config = { direction = 10 } },
 					["<C-f>"] = { "scroll_preview", config = { direction = -10 } },
 				},
-				]]
 			},
+		    ]]
 		})
 	end,
 }
--- nvim/.config/nvim/lua/plugins/tokyonight.lua
+++ nvim/.config/nvim/lua/plugins/tokyonight.lua
@@ -1,12 +1,12 @@
 return {
-	--"folke/tokyonight.nvim",
-	"ellisonleao/gruvbox.nvim",
+	"folke/tokyonight.nvim",
+	--"ellisonleao/gruvbox.nvim",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		--vim.cmd([[colorscheme tokyonight]])
+		vim.cmd([[colorscheme tokyonight]])
 
-		vim.o.background = "dark"
-		vim.cmd([[colorscheme gruvbox]])
+		--vim.o.background = "dark"
+		--vim.cmd([[colorscheme gruvbox]])
 	end,
 }
