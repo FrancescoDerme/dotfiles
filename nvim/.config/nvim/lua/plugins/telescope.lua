9dc2ab72e5be72a7680cdd2e2f8799a91338c897
Francesco Derme <francesco.derme02@gmail.com>
2026-02-02 10:55:16 +0100

    Navigation in telescope windows

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -8,6 +8,20 @@ return {
 	},
 
 	config = function()
+		local telescope = require("telescope")
+		local actions = require("telescope.actions")
+
+		telescope.setup({
+			defaults = {
+				mappings = {
+					i = {
+						["<M-j>"] = actions.move_selection_next,
+						["<M-k>"] = actions.move_selection_previous,
+					},
+				},
+			},
+		})
+
 		require("telescope").load_extension("fzf")
 	end,
 }
