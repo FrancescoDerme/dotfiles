e4124c64623fd5a6b60aceff5cf841dac06c0cf8
Francesco Derme <francesco.derme02@gmail.com>
2026-05-02 10:52:00 +0200

    Working with Latex

--- nvim/.config/nvim/lua/plugins/vimtex.lua
+++ nvim/.config/nvim/lua/plugins/vimtex.lua
@@ -14,6 +14,9 @@ return {
 		-- Keympas are setup manually so that they get picked up by Which-Key
 		vim.g.vimtex_mappings_enabled = 0
 
+		-- Don't forward search when compiling for the first time
+		vim.g.vimtex_view_forward_search_on_start = 0
+
 		vim.g.vimtex_compiler_latexmk = {
 			out_dir = "build",
 		}
