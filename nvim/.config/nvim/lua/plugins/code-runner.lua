2faffa01bd995e4caef4123b180723f7f3e577e4
Francesco Derme <francesco.derme02@gmail.com>
2026-06-09 11:40:19 +0200

    safer compilation rules

--- nvim/.config/nvim/lua/plugins/code-runner.lua
+++ nvim/.config/nvim/lua/plugins/code-runner.lua
@@ -1,13 +1,13 @@
 return {
-	"CRAG666/code_runner.nvim",
-	config = function()
-		require("code_runner").setup({
-			mode = "toggleterm",
-			filetype = {
-				cpp = "cd $dir && g++ -std=c++23 -DLOCAL -I$HOME/cp $fileName -o /tmp/$fileNameWithoutExt && /tmp/$fileNameWithoutExt",
-				c = "cd $dir && gcc $fileName -o /tmp/$fileNameWithoutExt && /tmp/$fileNameWithoutExt",
-				python = "python3 -u",
-			},
-		})
-	end,
+    "CRAG666/code_runner.nvim",
+    config = function()
+        require("code_runner").setup({
+            mode = "toggleterm",
+            filetype = {
+                cpp = "cd $dir && g++ -std=c++23 -DLOCAL -O2 -I$HOME/cp $fileName -o /tmp/$fileNameWithoutExt && /tmp/$fileNameWithoutExt",
+                c = "cd $dir && gcc $fileName -o /tmp/$fileNameWithoutExt && /tmp/$fileNameWithoutExt",
+                python = "python3 -u",
+            },
+        })
+    end,
 }
