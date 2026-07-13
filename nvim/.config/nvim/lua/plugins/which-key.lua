8d8a919ee462fa171d17ff1e7dd53558018a5692
Francesco Derme <francesco.derme02@gmail.com>
2026-07-13 16:57:51 +0200

    Whichkey plugin formatting

--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -1,50 +1,50 @@
 return {
-	"folke/which-key.nvim",
-	event = "VeryLazy",
-	opts = {
-		---@param mapping wk.Mapping
-		filter = function(mapping)
-			-- Hide DeadKeys, these are defined in "keymaps.lua"
-			if _G.DeadKeys then
-				for _, dead_key in ipairs(_G.DeadKeys) do
-					if mapping.lhs == dead_key then
-						return false
-					end
-				end
-			end
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+        ---@param mapping wk.Mapping
+        filter = function(mapping)
+            -- Hide DeadKeys, these are defined in "keymaps.lua"
+            if _G.DeadKeys then
+                for _, dead_key in ipairs(_G.DeadKeys) do
+                    if mapping.lhs == dead_key then
+                        return false
+                    end
+                end
+            end
 
-			-- Hide keys whose description starts with "Disabled"
-			if mapping.desc and mapping.desc:match("^Disable") then
-				return false
-			end
+            -- Hide keys whose description starts with "Disabled"
+            if mapping.desc and mapping.desc:match("^Disable") then
+                return false
+            end
 
-			return mapping.desc and mapping.desc ~= ""
-		end,
-		win = {
-			height = { min = 4, max = 8 },
-		},
-		spec = {
-			{
-				mode = { "n" },
-				{ "<leader>", group = "Keymaps" },
-				{ "<leader>d", group = "Directory" },
-				{ "<leader>l", group = "LSP" },
-				{ "<leader>f", group = "Find" },
-				{ "<leader>c", group = "Competitest" },
-				{ "<leader>cd", group = "Download" },
-				{ "<leader>o", group = "Oil" },
-				{ "]", group = "Next" },
-				{ "[", group = "Previous" },
-			},
-		},
-	},
-	keys = {
-		{
-			"<leader>?",
-			function()
-				require("which-key").show({ global = false })
-			end,
-			desc = "",
-		},
-	},
+            return mapping.desc and mapping.desc ~= ""
+        end,
+        win = {
+            height = { min = 4, max = 8 },
+        },
+        spec = {
+            {
+                mode = { "n" },
+                { "<leader>", group = "Keymaps" },
+                { "<leader>d", group = "Directory" },
+                { "<leader>l", group = "LSP" },
+                { "<leader>f", group = "Find" },
+                { "<leader>c", group = "Competitest" },
+                { "<leader>cd", group = "Download" },
+                { "<leader>o", group = "Oil" },
+                { "]", group = "Next" },
+                { "[", group = "Previous" },
+            },
+        },
+    },
+    keys = {
+        {
+            "<leader>?",
+            function()
+                require("which-key").show({ global = false })
+            end,
+            desc = "",
+        },
+    },
 }
